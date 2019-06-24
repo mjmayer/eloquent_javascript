8eb72934c929d201518f7a322b5dca75a0d4eb22
Michael Mayer <mjmayer@ucdavis.edu>
2019-06-24 13:23:04 -0700

    Solved 4.3

--- 4_3_a_list.js
+++ 4_3_a_list.js
@@ -5,7 +5,7 @@ function arrayToList(array) {
       list = { "value": array[i], "rest": null};
     }
     else {
-      list = { "value": array[i], list };
+      list = { "value": array[i], rest: list };
     }
   }
   return list;
@@ -14,16 +14,25 @@ function arrayToList(array) {
 function listToArray(list) {
   array = [];
   for (let node = list; node; node=node.rest) {
-    console.log(node);
-    array = array + node["value"];
+    array.push(node["value"]);
   }
   return array;
+}
+
+function prepend(value, rest) {
+  let list = { "value": value, "rest": rest}
+  return list
+}
+
+function nth (list, number) {
+  let array = listToArray(list);
+  return array[number];
 }
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-//console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
